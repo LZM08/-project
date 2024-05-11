3f7edc8bac93cf743440fe86846e0f76386967bc
LZM08 <lizhiming1008@gmail.com>
2024-05-11 15:43:39 +0900

    project.git

---
+++ Spring Boot firstproject/firstproject/src/test/java/com/example/firstproject/FirstprojectApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.firstproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstprojectApplicationTests {
+	public static void main(String[] args){
+		System.out.println("ff");
+	}
+	@Test
+	void contextLoads() {
+	}
+
+}
